82ec8c2cf12e0d1c07ccdae4329720424d69894e
Baylon Aguiar <baylon@trf2.jus.br>
2015-03-19 17:17:52 -0300

    Publicação automática do projeto pelo eclipse

---
+++ publicar_TP.bat
@@ -0,0 +1,33 @@
+@echo off
+
+cd C:\desenvolvimento\workspace\siga-transportes\sigatp
+
+call mvn clean install -DskipTests -Dsiga.versao=1.1.1
+
+if %errorlevel% NEQ 0 (
+  echo.
+  echo.
+  echo.
+  echo Erro ao gerar pacote! Saindo...
+  goto end
+)
+
+call copy /Y ..\target\sigatp.war ..\..\..\jboss-eap-6.2.3\standalone\deployments\
+
+if %errorlevel% NEQ 0 (
+  echo.
+  echo.
+  echo.
+  echo Erro ao copiar pacote para o diretorio deployments do JBoss! Saindo...
+  goto end
+)
+
+echo.
+echo.
+echo.
+echo Projeto publicado com sucesso. Saindo...
+goto end
+
+
+:end
+endlocal
