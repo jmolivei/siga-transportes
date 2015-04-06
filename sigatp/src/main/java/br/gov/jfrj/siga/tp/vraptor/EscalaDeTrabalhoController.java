4168b092e07ce17be9cb2b1197e66a93a3c2f20a
Anderson Zanichelli <anderzanic@gmail.com>
2015-04-06 16:47:01 -0300

    [PRJ_TRF2][777] - OSI_FS0020 PRJ_OSI - 08 MIGRAÇÃO DO CONTROLLER ESCALASDETRABALHO

--- sigatp/src/main/java/br/gov/jfrj/siga/tp/vraptor/EscalaDeTrabalhoController.java
+++ sigatp/src/main/java/br/gov/jfrj/siga/tp/vraptor/EscalaDeTrabalhoController.java
@@ -1,19 +1,36 @@
 package br.gov.jfrj.siga.tp.vraptor;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.servlet.http.HttpServletRequest;
 
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.core.Localization;
-import br.gov.jfrj.siga.dp.dao.CpDao;
 import br.gov.jfrj.siga.tp.auth.AutorizacaoGI;
+import br.gov.jfrj.siga.tp.model.EscalaDeTrabalho;
+import br.gov.jfrj.siga.tp.model.TpDao;
 import br.gov.jfrj.siga.vraptor.SigaObjects;
 
+//@With(AutorizacaoGIAntigo.class)
+@Resource
 public class EscalaDeTrabalhoController extends TpController {
 
 	public EscalaDeTrabalhoController(HttpServletRequest request, Result result, Localization localization, SigaObjects so, AutorizacaoGI dadosAutorizacao, EntityManager em) throws Exception {
-		super(request, result, CpDao.getInstance(), localization, so, dadosAutorizacao, em);
+		super(request, result, TpDao.getInstance(), localization, so, dadosAutorizacao, em);
 	}
 
+	@Path("/app/escalaDeTrabalho/listar")
+	public void listar() {
+    	List<EscalaDeTrabalho> escalas = EscalaDeTrabalho.buscarTodasVigentes();
+        
+        result.include("escalas", escalas);
+    }
 	
+	@Path("/app/escalaDeTrabalho/editar/{id}")
+	public void editar(final Long id) {
+		
+	}
 }
