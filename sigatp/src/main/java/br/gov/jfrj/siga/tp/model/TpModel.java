5483777ab4acfb778c974e455fb8d71629e29f32
Heidi Kussakawa <heidiks@gmail.com>
2015-04-07 09:20:48 -0300

    [PRJ_TRF2] [776] - OSI_FS0020 PRJ_OSI - 07 MIGRAÇÃO DO CONTROLLER PLANTOES - Métodos para formatação de data

--- sigatp/src/main/java/br/gov/jfrj/siga/tp/model/TpModel.java
+++ sigatp/src/main/java/br/gov/jfrj/siga/tp/model/TpModel.java
@@ -1,8 +1,11 @@
 package br.gov.jfrj.siga.tp.model;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import br.gov.jfrj.siga.model.Objeto;
 
-public abstract class TpModel extends Objeto {
+public class TpModel extends Objeto {
 
 	private static final long serialVersionUID = -3265658962532346951L;
 
@@ -12,4 +15,17 @@ public abstract class TpModel extends Objeto {
 	public void save() {
 		TpDao.getInstance().gravar(this);
 	}
+	
+	public String formatDateDDMMYYYY(Calendar cal) {
+		return formatDate(cal, "dd/MM/yyyy");
+	}
+	
+	public String formatDateDDMMYYYYHHMM(Calendar cal) {
+		return formatDate(cal, "dd/MM/yyyy HH:mm");
+	}
+
+	private String formatDate(Calendar cal, String formato) {
+		return new SimpleDateFormat(formato).format(cal.getTime());
+	}
+	
 }
