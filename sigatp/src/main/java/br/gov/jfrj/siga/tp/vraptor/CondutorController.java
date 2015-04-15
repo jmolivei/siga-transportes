6d8a09d3f45e70c512e7400512286e3cc4fd05bc
Heidi Kussakawa <heidiks@gmail.com>
2015-04-15 08:32:44 -0300

    [PRJ_TRF2] [774] - OSI_FS0020 PRJ_OSI - 05 MIGRAÇÃO DO CONTROLLER CONDUTORES

--- sigatp/src/main/java/br/gov/jfrj/siga/tp/vraptor/CondutorController.java
+++ sigatp/src/main/java/br/gov/jfrj/siga/tp/vraptor/CondutorController.java
@@ -2,29 +2,31 @@ package br.gov.jfrj.siga.tp.vraptor;
 
 import java.io.ByteArrayInputStream;
 import java.util.List;
-
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceException;
 import javax.servlet.http.HttpServletRequest;
-
+import javax.validation.Valid;
 import org.apache.log4j.Logger;
-
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.Validator;
 import br.com.caelum.vraptor.core.Localization;
+import br.com.caelum.vraptor.validator.I18nMessage;
+import br.com.caelum.vraptor.validator.ValidationMessage;
+import br.com.caelum.vraptor.view.Results;
 import br.gov.jfrj.siga.dp.DpPessoa;
 import br.gov.jfrj.siga.dp.dao.CpDao;
-import br.gov.jfrj.siga.tp.auth.annotation.RoleAdmin;
-import br.gov.jfrj.siga.tp.auth.annotation.RoleAdminMissao;
-import br.gov.jfrj.siga.tp.auth.annotation.RoleAdminMissaoComplexo;
 import br.gov.jfrj.siga.tp.model.CategoriaCNH;
 import br.gov.jfrj.siga.tp.model.Condutor;
+import br.gov.jfrj.siga.tp.model.Imagem;
 import br.gov.jfrj.siga.tp.model.ItemMenu;
 import br.gov.jfrj.siga.tp.model.TpDao;
 import br.gov.jfrj.siga.vraptor.SigaObjects;
 
 @Resource
+@Path("/app/condutor")
 public class CondutorController extends TpController {
 	
 	public CondutorController(HttpServletRequest request, Result result, CpDao dao, Localization localization, Validator validator, SigaObjects so, EntityManager em) throws Exception {
@@ -33,46 +35,127 @@ public class CondutorController extends TpController {
 
 	private static Logger logger = Logger.getLogger(CondutorController.class);
 	
-	@Path("/app/condutor/listar")
-	public void listar() {
+	@Path("/listar")
+	public void listar() throws Exception {
 		result.include("condutores", getCondutores());
 	}
 
-	@RoleAdmin
-	@RoleAdminMissao
-	@RoleAdminMissaoComplexo
-	@Path("/app/condutor/editar/{id}")
-	public void edita(Long id) {
-		try {
-			Condutor condutor = Condutor.AR.findById(id);
-
-			if (condutor.getDpPessoa() != null) {
-				condutor.setDpPessoa(recuperaPessoa(condutor.getDpPessoa()));
-			} else {
-				condutor.setDpPessoa(new DpPessoa());
-			}
-
-			result.include("condutor", condutor);
-			result.include("categoriaCNH", condutor.getCategoriaCNH().getDescricao());
-			result.include("listCategorias", CategoriaCNH.values());
-			result.include("imgArquivo", new ByteArrayInputStream(condutor.getConteudoimagemblob()));
-			
-			MenuMontador.instance(result).recuperarMenuCondutores(id, ItemMenu.DADOSCADASTRAIS);
-
-		} catch (Exception e) {
-			logger.error(e, e.getCause());
-		}
-	}
+//	@Path("/app/condutor/listarComMensagem")
+//	public void listarComMensagem(String mensagem) throws Exception {
+//		List<Condutor> condutores = Condutor.listarTodos(AutorizacaoGIAntigo.titular().getOrgaoUsuario());
+//		Validation.addError("condutor", mensagem);
+//		renderTemplate("@listar", condutores);
+//	}
 	
+//	@RoleAdmin
+//	@RoleAdminMissao
+//	@RoleAdminMissaoComplexo
+	@Path("/editar/{id}")
+	public void editar(Long id) throws Exception {
+		Condutor condutor;
+		if(id > 0)  {
+			condutor = Condutor.AR.findById(id);
+			if (condutor.getDpPessoa() != null) 
+				condutor.setDpPessoa(recuperaPessoa(condutor.getDpPessoa()));
+			else 
+				condutor.setDpPessoa(new DpPessoa());
+			
+			result.include("categoriaCNH", condutor.getCategoriaCNH().getDescricao());
+			
+			if(condutor.getConteudoimagemblob() != null)
+				result.include("imgArquivo", new ByteArrayInputStream(condutor.getConteudoimagemblob()));
+			
+		} else 
+			condutor = new Condutor();
 
-	@Path("/app/condutor/excluir")
-	public void exclui(Long id) {
-		System.out.println();
+		MenuMontador.instance(result).recuperarMenuCondutores(id, ItemMenu.DADOSCADASTRAIS);
+		result.include("listCategorias", CategoriaCNH.values());
+		result.include("condutor", condutor);
 	}
 
-	@Path("/app/condutor/incluir")
-	public void inclui() {
-		System.out.println();
+//	@RoleAdmin
+//	@RoleAdminMissao
+//	@RoleAdminMissaoComplexo
+	@Path("/salvar")
+	public void salvar(@Valid Condutor condutor) throws Exception {
+		condutor.setDpPessoa(DpPessoa.AR.findById(condutor.getDpPessoa().getId()));
+		if (condutor.getArquivo() != null) {
+			error(!Imagem.tamanhoImagemAceito(condutor.getArquivo().blob.length), "imagem", "condutor.tamanhoImagemAceito.validation");
+			// && !condutor.arquivo.nomeArquivo.contains("pdf")) {
+			error(!condutor.getArquivo().mime.startsWith("image"), "imagem", "condutores.arquivoImagem.validation");
+			condutor.setConteudoimagemblob(condutor.getArquivo().blob);
+		} else {
+			//TODO  HD verificar!
+//			if (condutor.getSituacaoImagem().equals("semimagem")) 
+//				condutor.setConteudoimagemblob(null);
+		}
+		
+		error(condutor.getDpPessoa() == null, "dpPessoa", "condutor.dppessoa.validation");
+		
+		condutor.setCpOrgaoUsuario(getTitular().getOrgaoUsuario());
+
+		if (validator.hasErrors()) {
+			if (condutor.getDpPessoa() != null) 
+				condutor.setDpPessoa(recuperaPessoa(condutor.getDpPessoa()));
+			else 
+				condutor.setDpPessoa(new DpPessoa());
+			
+			result.include("condutor", condutor);
+			if(condutor.getId() > 0) 
+				result.forwardTo(this).editar(condutor.getId());
+			else
+				result.forwardTo(this).incluir();
+		}
+
+		condutor.save();
+		result.forwardTo(this).listar();
+	}
+
+//	@RoleAdmin
+//	@RoleAdminMissao
+//	@RoleAdminMissaoComplexo
+	@Path("/excluir/{id}")
+	public void excluir(Long id) throws Exception {
+		EntityTransaction tx = Condutor.AR.em().getTransaction();
+		Condutor condutor = Condutor.AR.findById(id);
+
+		if (!tx.isActive()) 
+			tx.begin();
+
+		try {
+			condutor.delete();
+			tx.commit();
+			result.forwardTo(this).listar();
+		} catch (PersistenceException ex) {
+			tx.rollback();
+			if (ex.getCause().getCause().getMessage().contains("restrição de integridade")) {
+				//TODO  HD mensagem!
+//				listarComMensagem("condutor.excluir.validation");
+//				result.forwardTo(this).listar();
+				validator.add(new I18nMessage("condutor", "condutor.excluir.validation"));
+			} else {
+				//TODO  HD mensagem!
+//				listarComMensagem(ex.getMessage());
+				validator.add(new ValidationMessage(ex.getMessage(), "condutor"));
+			}
+			validator.onErrorUse(Results.logic()).forwardTo(CondutorController.class).listar();
+		} catch (Exception ex) {
+			tx.rollback();
+			//TODO  HD mensagem!
+//			listarComMensagem(ex.getMessage());
+			validator.add(new ValidationMessage(ex.getMessage(), "condutor"));
+			validator.onErrorUse(Results.logic()).forwardTo(CondutorController.class).listar();
+		}
+		
+		result.forwardTo(this).listar();
+	}
+
+//	@RoleAdmin
+//	@RoleAdminMissao
+//	@RoleAdminMissaoComplexo
+	@Path("/incluir")
+	public void incluir() throws Exception {
+		result.forwardTo(this).editar(0L);
 	}
 
 	private DpPessoa recuperaPessoa(DpPessoa dpPessoa) throws Exception {
@@ -84,186 +167,32 @@ public class CondutorController extends TpController {
 		try {
 			List<Condutor> condutores = Condutor.listarTodos(getTitular().getOrgaoUsuario());
 			return condutores;
-
 		} catch (Exception ignore) {
 			return null;
 		}
 	}
 	
-//	private void getImagem(Long id) {
-//	if (id != null) {
-//		// Pesquisar Imagem por id
-//		// Imagem arq = Imagem.newInstance(file);
-//		// renderBinary(new ByteArrayInputStream(arq.blob),
-//		// arq.nomeArquivo);
-//		Condutor condutor = Condutor.AR.findById(id);
-//		renderBinary(new ByteArrayInputStream(condutor.conteudoimagemblob),	condutor.conteudoimagemblob.length);
-//	}
-//}S
-
+	//TODO  HD public page?
+	public void getImagem(Long id) throws Exception {
+		if (id != null) {
+			// Pesquisar Imagem por id
+			// Imagem arq = Imagem.newInstance(file);
+			// renderBinary(new ByteArrayInputStream(arq.blob),
+			// arq.nomeArquivo);
+			Condutor condutor = Condutor.AR.findById(id);
+			result.include("conteudoImagemBlob" ,new ByteArrayInputStream(condutor.getConteudoimagemblob()));
+			result.include("conteudoImagemBlobTamanho" , condutor.getConteudoimagemblob().length);
+		}
+	}
+	
+//	@RoleAdmin
+//	@RoleAdminMissao
+//	@RoleAdminMissaoComplexo
+	@Path("/exibirDadosDpPessoa/{idPessoa}")
+	public void exibirDadosDpPessoa(Long idPessoa) throws Exception {
+		DpPessoa pessoa = DpPessoa.AR.findById(idPessoa);
+		result.include("pessoa", pessoa);
+	}
 	
 
-	
-	
-//
-//	public void salvar() {
-//		
-//	}
-//
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//
-//	private static final String ACTION_EDITAR = "@editar";
-//	private static final String ACTION_INCLUIR = "@incluir";
-//
-//	public static void listar() throws Exception {
-//		List<Condutor> condutores = Condutor.listarTodos(AutorizacaoGI.titular().getOrgaoUsuario());
-//		render(condutores);
-//	}
-//
-//	public static void listarComMensagem(String mensagem) throws Exception {
-//		List<Condutor> condutores = Condutor.listarTodos(AutorizacaoGI.titular().getOrgaoUsuario());
-//		Validation.addError("condutor", mensagem);
-//		renderTemplate("@listar", condutores);
-//	}
-//
-//	@RoleAdmin
-//	@RoleAdminMissao
-//	@RoleAdminMissaoComplexo
-//	public static void excluir(Long id) throws Exception {
-//		EntityTransaction tx = Condutor.em().getTransaction();
-//		Condutor condutor = Condutor.findById(id);
-//
-//		if (!tx.isActive()) {
-//			tx.begin();
-//		}
-//
-//		try {
-//			condutor.delete();
-//			tx.commit();
-//			listar();
-//
-//		} catch (PersistenceException ex) {
-//			tx.rollback();
-//			if (ex.getCause().getCause().getMessage().contains("restrição de integridade")) {
-//				listarComMensagem("condutor.excluir.validation");
-//			} else {
-//				listarComMensagem(ex.getMessage());
-//			}
-//		} catch (Exception ex) {
-//			tx.rollback();
-//			listarComMensagem(ex.getMessage());
-//		}
-//
-//		listar();
-//	}
-//
-//	@RoleAdmin
-//	@RoleAdminMissao
-//	@RoleAdminMissaoComplexo
-//	public static void editar(Long id) throws Exception {
-//		Condutor condutor = Condutor.findById(id);
-//
-//		if (condutor.dpPessoa != null) {
-//			condutor.dpPessoa = recuperaPessoa(condutor.dpPessoa);
-//		} else {
-//			condutor.dpPessoa = new DpPessoa();
-//		}
-//		MenuMontador.instance().RecuperarMenuCondutores(id, ItemMenu.DADOSCADASTRAIS);
-//		render(condutor);
-//	}
-//
-//	private static DpPessoa recuperaPessoa(DpPessoa dpPessoa) throws Exception {
-//		return 	DpPessoa.AR.find("idPessoaIni = ? and dataFimPessoa = null",dpPessoa.getIdInicial()).first();
-//	}
-//
-//	@RoleAdmin
-//	@RoleAdminMissao
-//	@RoleAdminMissaoComplexo
-//	public static void incluir() {
-//		Condutor condutor = new Condutor();
-//		MenuMontador.instance().RecuperarMenuCondutores(new Long(0),ItemMenu.DADOSCADASTRAIS);
-//		render(condutor);
-//	}
-//
-//	@RoleAdmin
-//	@RoleAdminMissao
-//	@RoleAdminMissaoComplexo
-//	public static void salvar(Condutor condutor) throws Exception {
-//		validation.valid(condutor);
-//
-//		if (condutor.arquivo != null) {
-//			if (!Imagem.tamanhoImagemAceito(condutor.arquivo.blob.length)) {
-//				Validation.addError("imagem", "condutor.tamanhoImagemAceito.validation");
-//			}
-//
-//			if (!condutor.arquivo.mime.startsWith("image")) { // && !condutor.arquivo.nomeArquivo.contains("pdf")) {
-//				Validation.addError("imagem", "condutores.arquivoImagem.validation", condutor.arquivo.mime);
-//			}
-//			condutor.conteudoimagemblob = condutor.arquivo.blob;
-//		} else {
-//			if (condutor.situacaoImagem.equals("semimagem")) {
-//				condutor.conteudoimagemblob = null;
-//			}
-//		}
-//		
-//		if (condutor.dpPessoa == null) {
-//			Validation.addError("dpPessoa", "condutor.dppessoa.validation");
-//		}
-//
-//		condutor.cpOrgaoUsuario = AutorizacaoGI.titular().getOrgaoUsuario();
-//
-//		if (Validation.hasErrors()) {
-//			if (condutor.dpPessoa != null) {
-//				condutor.dpPessoa = recuperaPessoa(condutor.dpPessoa);
-//			} else {
-//				condutor.dpPessoa = new DpPessoa();
-//			}
-//			renderTemplate((condutor.id == 0 ? CondutorController.ACTION_INCLUIR : CondutorController.ACTION_EDITAR), condutor);
-//		}
-//
-//		condutor.save();
-//		listar();
-//	}
-//
-//	public static void exibirDadosDpPessoa(DpPessoa pessoa) throws Exception {
-//		render(recuperaPessoa(pessoa));
-//	}
-//
-//	public static void getImagem(Long id) {
-//		if (id != null) {
-//			// Pesquisar Imagem por id
-//			// Imagem arq = Imagem.newInstance(file);
-//			// renderBinary(new ByteArrayInputStream(arq.blob),
-//			// arq.nomeArquivo);
-//			Condutor condutor = Condutor.findById(id);
-//			renderBinary(new ByteArrayInputStream(condutor.conteudoimagemblob),	condutor.conteudoimagemblob.length);
-//		}
-//	}
-//
-//	public static void exibirImgArquivo(Long id) {
-//		Condutor condutor = Condutor.findById(id);
-//		renderText(condutor.conteudoimagemblob != null ? true : false);
-//	}
-//
-//	public static void exibirImagem(Long id) {
-//		/*
-//		 * if (file != null) { Imagem arq = Imagem.newInstance(file);
-//		 * renderTemplate("@exibirImagem", arq.blob); }
-//		 */
-//		Condutor condutor = Condutor.findById(id);
-//		renderTemplate("@exibirImagem", condutor);
-//	}
 }
