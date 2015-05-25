6a51fdaea36c500a38e664125944f928eccc2f58
Anderson Zanichelli <anderson.zanichelli@db1.com>
2015-05-25 14:26:01 -0300

    [PRJ_TRF2][958] - OSI_FS0022 - SIGA-TP PRJ_OSI - 01 ADAPTAÇÃO DO PROJETO PLAY PARA O USO DO FRAMEWORK VRAPTOR Migração da tag link

---
+++ sigatp/src/main/java/br/gov/jfrj/siga/tp/tag/LinkTag.java
@@ -0,0 +1,80 @@
+package br.gov.jfrj.siga.tp.tag;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import br.com.caelum.vraptor.validator.I18nMessage;
+import br.gov.jfrj.siga.tp.util.FormataCaminhoDoContextoUrl;
+
+public class LinkTag extends TagSupport {
+
+	private static final long serialVersionUID = -333366995935848194L;
+	private static final String IMG_LINKNOVAJANELAICON = "/sigatp/public/images/linknovajanelaicon.png";
+
+	private String texto;
+	private String parteTextoLink;
+	private String comando;
+	private String comandoEditavel = "";
+	private boolean ehEditavel;
+
+	@Override
+	public int doStartTag() throws JspException {
+		try {
+			validarAtributos();
+			gerarConteudoTag();
+		} catch (IOException e) {
+			throw new JspException(e);
+		}
+
+		return EVAL_PAGE;
+	}
+
+	private void gerarConteudoTag() throws IOException {
+		JspWriter out = pageContext.getOut();
+
+		if (ehEditavel)
+			comandoEditavel = comando.replace("true", "false");
+
+		if (!comandoEditavel.isEmpty())
+			out.append("<a href='" + comandoEditavel + "'>" + parteTextoLink + "</a>");
+		else
+			out.append(parteTextoLink);
+
+		FormataCaminhoDoContextoUrl formata = new FormataCaminhoDoContextoUrl();
+		String caminhoUrl = formata.retornarCaminhoContextoUrl(comando);
+		out.append(" <a href=\"#\" onclick=\"javascript:window.open('" + caminhoUrl + "');\">");
+		out.append("<img src=\"" + IMG_LINKNOVAJANELAICON + "\" alt=\"Abrir em uma nova janela\" title=\"Abrir em uma nova janela\"></a> ");
+		out.append(texto.replace(parteTextoLink, ""));
+
+		out.println();
+		out.flush();
+	}
+
+	private void validarAtributos() throws JspException {
+		if((null == comando || "".equals(comando)) || (null == parteTextoLink || "".equals(parteTextoLink)))
+			throw new JspException(new I18nMessage("tagLib","tpTags.parametrosInvalidos.exception").getMessage());
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public void setParteTextoLink(String parteTextoLink) {
+		this.parteTextoLink = parteTextoLink;
+	}
+
+	public void setComando(String comando) {
+		this.comando = comando;
+	}
+
+	public void setComandoEditavel(String comandoEditavel) {
+		this.comandoEditavel = comandoEditavel;
+	}
+
+	public void setEhEditavel(Boolean ehEditavel) {
+		this.ehEditavel = ehEditavel;
+	}
+}
