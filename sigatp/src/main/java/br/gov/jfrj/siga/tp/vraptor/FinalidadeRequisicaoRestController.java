1fc598524e8d9199058b74bcec7674a7ce83d866
Leonardo Lopes <leonardo.lopes@db1.com.br>
2015-06-10 09:22:55 -0300

    [PRJ_TRF2][1037] - OSI_FS0022 - SIGA-TP PRJ_OSI - 20 MIGRAÇÃO DOS CONTROLLERS DE WEBSERVICE - ISSUES SONAR

--- sigatp/src/main/java/br/gov/jfrj/siga/tp/vraptor/FinalidadeRequisicaoRestController.java
+++ sigatp/src/main/java/br/gov/jfrj/siga/tp/vraptor/FinalidadeRequisicaoRestController.java
@@ -12,6 +12,7 @@ import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.Validator;
 import br.com.caelum.vraptor.view.Results;
 import br.gov.jfrj.siga.dp.dao.CpDao;
+import br.gov.jfrj.siga.tp.exceptions.RestControllerException;
 import br.gov.jfrj.siga.tp.model.TpDao;
 import br.gov.jfrj.siga.tp.rest.FinalidadeRequisicaoRest;
 import br.gov.jfrj.siga.vraptor.SigaObjects;
@@ -20,38 +21,42 @@ import br.gov.jfrj.siga.vraptor.SigaObjects;
 @Path("/app/finalidadeRequisicaoRest")
 public class FinalidadeRequisicaoRestController extends TpController {
 
-    public FinalidadeRequisicaoRestController(HttpServletRequest request, Result result,
-            CpDao dao, Validator validator, SigaObjects so, EntityManager em) {
+    private static final String FUNCAO_NAO_IMPLEMENTADA = "Funcao nao implementada";
+
+    public FinalidadeRequisicaoRestController(HttpServletRequest request, Result result, CpDao dao, Validator validator, SigaObjects so, EntityManager em) {
         super(request, result, TpDao.getInstance(), validator, so, em);
     }
 
     @Path("/ver/{id}")
-    public void ver(Long id) throws Exception {
-        ObjectMapper oM = new ObjectMapper();
-        ObjectWriter oW = oM.writer().withDefaultPrettyPrinter();
-        String json;
-        
-        FinalidadeRequisicaoRest fin = FinalidadeRequisicaoRest.buscarFinalidade(id);
-        if(fin != null) 
-            json = oW.writeValueAsString(fin);
-        else {
-            FinalidadeRequisicaoRest[] finalidades = FinalidadeRequisicaoRest.buscarFinalidades();
-            json = oW.writeValueAsString(finalidades);
-        }
+    public void ver(Long id) throws RestControllerException {
+        try {
+            ObjectMapper oM = new ObjectMapper();
+            ObjectWriter oW = oM.writer().withDefaultPrettyPrinter();
+            String json;
 
-        result.use(Results.http()).body(json);
+            FinalidadeRequisicaoRest fin = FinalidadeRequisicaoRest.buscarFinalidade(id);
+            if (fin != null)
+                json = oW.writeValueAsString(fin);
+            else {
+                FinalidadeRequisicaoRest[] finalidades = FinalidadeRequisicaoRest.buscarFinalidades();
+                json = oW.writeValueAsString(finalidades);
+            }
+
+            result.use(Results.http()).body(json);
+        } catch (Exception e) {
+            throw new RestControllerException(e);
+        }
     }
-    
-    public void incluir() throws Exception {
-        result.use(Results.http()).body("Funcao nao implementada");
+
+    public void incluir() {
+        result.use(Results.http()).body(FUNCAO_NAO_IMPLEMENTADA);
     }
-    
-    public void alterar(Long id) throws Exception {
-        result.use(Results.http()).body("Funcao nao implementada");
+
+    public void alterar() {
+        result.use(Results.http()).body(FUNCAO_NAO_IMPLEMENTADA);
     }
-    
-    public void excluir(Long id) throws Exception {
-        result.use(Results.http()).body("Funcao nao implementada");
+
+    public void excluir() {
+        result.use(Results.http()).body(FUNCAO_NAO_IMPLEMENTADA);
     }
 }
-
